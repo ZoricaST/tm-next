ea7dd1f22e007db2d0fc40b3b7958e91ed3f213d
Zorica <zoricagithub@gmail.com>
2021-12-21 08:50:02 +0100

    id

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 import Head from 'next/head'
-export default function Home() {
-  return (
-    <div >
-    <Head>
-    <title> WebDEv News</title>
-    <meta name='keywords' content='web development, programming'/>
-    </Head>
-    <h1>Dobrodosli u Next</h1>
-    </div>
-  )
+import Image from 'next/image'
+import styles from '../styles/Layout.module.css'
+
+export default function Home({articles}) {
+console.log(articles)
+return (
+<div >
+<Head>
+<title> WebDEv News</title>
+<meta name='keywords' content='web development, programming'/>
+</Head>
+
+</div>
+)
+}
+
+export const getStaticProps = async () => {
+const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+const articles = await res.json()
+
+return {
+props: {
+articles,
+},
+}
 }
