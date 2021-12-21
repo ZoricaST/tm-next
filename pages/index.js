c0bc44929734957183e7d3847cc68252ab322462
Zorica <zoricagithub@gmail.com>
2021-12-21 09:54:27 +0100

    gotovo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
+import {server} from '../config'
 import Image from 'next/image'
 import styles from '../styles/Layout.module.css'
-
+import ArticleList from '../components/ArticlesList'
 export default function Home({articles}) {
 console.log(articles)
 return (
@@ -10,18 +11,29 @@ return (
 <title> WebDEv News</title>
 <meta name='keywords' content='web development, programming'/>
 </Head>
-
+<ArticleList articles={articles} />
 </div>
 )
 }
 
 export const getStaticProps = async () => {
-const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
-const articles = await res.json()
-
-return {
-props: {
-articles,
-},
-}
-}
+  const res = await fetch(`${server}/api/articles`)
+  const articles = await res.json()
+  
+  return {
+  props: {
+  articles,
+  },
+  }
+  }
+  
+  // export const getStaticProps = async () => {
+  // const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  // const articles = await res.json()
+  
+  // return {
+  // props: {
+  // articles,
+  // },
+  // }
+  // }
